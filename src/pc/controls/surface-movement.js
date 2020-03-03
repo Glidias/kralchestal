b8f3049f97dd2a090bf420703f960ad32d88d38f
Glidias <glidias@hotmail.com>
2020-03-03 17:43:24 +0800

    surface movement script finalised for general usage

--- src/pc/controls/surface-movement.js
+++ src/pc/controls/surface-movement.js
@@ -1,13 +1,15 @@
 import Vector3D from "../../hx/jeash/geom/Vector3D";
-import MoveResult from "../../hx/components/MoveResult";
 import EllipsoidCollider from "../../hx/systems/collisions/EllipsoidCollider";
 import CollisionBoundNode from "../../hx/altern/collisions/CollisionBoundNode";
+import CollisionEvent from "../../hx/systems/collisions/CollisionEvent";
 
 /**
- * Simplified surface movement relying on external supplied velocity
+ * Simplified surface movement often relying on external supplied velocity
  * without calculating exact collision ground normal value for relatively stable movement.
  * ground movement.
+ * @type {typeof pc.ScriptType & {attributes: pc.ScriptAttributes}}
  */
+
 var SurfaceMovement = pc.createScript('surfaceMovement');
 
 SurfaceMovement.attributes.add('gravForce', { type: 'number', default:0.5 });
@@ -17,21 +19,21 @@ SurfaceMovement.attributes.add('groundNormalThreshold', { type: 'number', defaul
 if (typeof importScripts !== 'function') {
     SurfaceMovement.SOURCE = new Vector3D();
     SurfaceMovement.DISPLACEMENT  = new Vector3D();
-    SurfaceMovement.MOVE_RESULT = new MoveResult();
+    SurfaceMovement.COLLIDER  =  new EllipsoidCollider(0.6, 1.8, 0.6, 0.000001, true);
 }
 
 SurfaceMovement.prototype.initialize = function() {
     this.gotGroundNormal = false;
-    this.vel = new pc.Vec3(); // internal velocity for surfaceMovement to take into account accumlated gravity
-    this.extVelocity = null; // optional external velocity to track x and z values
-    this.collider = new EllipsoidCollider(this.entity.collision.radius, this.entity.collision.height/2, this.entity.collision.radius, this.ellipsoidThreshold, true);
+    // Other scripts might have already initialized .posNode and .vel velocity for this component
+    if (!this.posNode) this.posNode = this.entity;
+    if (!this.vel) this.vel = new pc.Vec3();
 };
 
 // initialize code called once per entity
 SurfaceMovement.prototype.postInitialize = function() {
     this.collisionScene = this.app.root.collisionScene || (this.app.root.children.length ? this.app.root.children[0].collisionScene : null) || ( window["Rootscene"] ? Rootscene.SCENE :  null);
     if (!this.collisionScene) alert("SurfaceMovement:: Failed to find collision scene!");
-    if (this.collisionScene == null) { 
+    if (this.collisionScene == null) {
         console.warn("surface-movement :: Collision scene currently empty!");
         this.collisionScene = CollisionBoundNode.create(new Transform3D());
         this.app.once('collision-scene-initialized', this.onCollisionSceneInited, this);
@@ -44,64 +46,62 @@ SurfaceMovement.prototype.onCollisionSceneInited = function(scene) {
 
 // update code called every frame
 SurfaceMovement.prototype.update = function(dt) {
-    var pos = this.entity.getPosition();
+    var pos = this.posNode.getPosition();
 
-    var SM = SurfaceMovement;
-    var moveResult = SM.MOVE_RESULT;
-    var displacement = SM.DISPLACEMENT;
-    var source = SM.SOURCE;
+    /**
+     * @type {Vector3D}
+     */
+    var displacement = SurfaceMovement.DISPLACEMENT;
+
+    /**
+     * @type {Vector3D}
+     */
+    var source = SurfaceMovement.SOURCE;
+
+    /**
+     * @type {EllipsoidCollider}
+     */
+    var collider = SurfaceMovement.COLLIDER;
+    collider.radiusX = collider.radiusZ = this.entity.collision.radius;
+    collider.radiusY = this.entity.collision.height * 0.5;
+
     // || (this.gotGroundNormal) &&  .maximum_ground_normal.dotProduct(this.vel) > 0)
     if ( !this.gotGroundNormal ) {
          this.vel.y -= this.gravForce;
     }
-    
-    if (this.extVelocity) {
-        this.vel.x = this.extVelocity.x;
-        //is.vel.y += this.extVelocity.y;
-        this.vel.z = this.extVelocity.z;
-    }
+
     if (this.vel.y > 0) this.vel.y = 0;
-    
+
     source.x = pos.x;
     source.y = pos.y;
     source.z = pos.z;
-    displacement.x =this.vel.x * dt;
+    displacement.x = this.vel.x * dt;
     displacement.y = this.vel.y * dt;
     displacement.z = this.vel.z * dt;
-    
-    var destination = this.collider.calculateDestination(source, displacement, this.collisionScene);
-    moveResult.collisions = this.collider.collisions;
-    this.collider.collisions = null;
 
+
+    var destination = collider.calculateDestination(source, displacement, this.collisionScene);
     var gotGroundNormal = false; // this.gotGroundNormal;
-  
+
    if (this.vel.x*this.vel.x + this.vel.y+this.vel.y + this.vel.z*this.vel.z !== 0) {
         this.gotGroundNormal = false;
    }
-  
-    var c = moveResult.collisions;
-    
-    while (c != null) { // consiedr early out and global gotGroundNormal check?
-        //console.log("C:"+c.normal.y);
-        if (c.normal.y >= this.groundNormalThreshold ) { 
-           this.vel.y = 0;
+
+    var c = collider.collisions;
+    collider.collisions = null;
+
+    var nextEvent;
+    while (c != null) {
+        nextEvent = c.next;
+        if (!gotGroundNormal && c.normal.y >= this.groundNormalThreshold ) {
            gotGroundNormal = true;
-           break;   
+           // break;
         }
-        c = c.next;
+        c.next = CollisionEvent.COLLECTOR;
+        CollisionEvent.COLLECTOR = c;
+        c = nextEvent;
     }
+
     this.gotGroundNormal = gotGroundNormal;
-
-    var invT  = 1/dt;
-    if (moveResult.collisions != null) {    
-        this.vel.x = (destination.x - pos.x) * invT;
-        this.vel.y = (destination.y - pos.y) * invT; 
-        this.vel.z = (destination.z - pos.z) * invT; 
-        moveResult.disposeCollisions();
-    }
-
-    pos.x = destination.x;
-    pos.y = destination.y;
-    pos.z = destination.z;
-    this.entity.setPosition(pos.x, pos.y, pos.z);
+    this.posNode.setPosition(destination.x, destination.y, destination.z);
 };
