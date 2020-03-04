08a2ce03bb7c951728659ab1a39939751c491fde
Glenn Ko <glidias@hotmail.com>
2020-03-05 01:05:22 +0800

    reinstate surfaceagent

---
+++ src/pc/yuka/entities/surface-agent.ts
@@ -0,0 +1,97 @@
+import { Vehicle } from "../../../../yuka/src/steering/Vehicle";
+import EllipsoidCollider from "../../../hx/systems/collisions/EllipsoidCollider";
+import CollisionBoundNode from "../../../hx/altern/collisions/CollisionBoundNode";
+import Vector3D from "../../../hx/jeash/geom/Vector3D";
+import CollisionEvent from "../../../hx/systems/collisions/CollisionEvent";
+
+const DISPLACEMENT = new Vector3D(0);
+const SOURCE = new Vector3D(0);
+//const TARGET_DIRECTION = new Vector3();
+
+class SurfaceAgent extends Vehicle {
+   collider: EllipsoidCollider;
+   collidable: CollisionBoundNode;
+
+   groundNormalThreshold: number;
+   gravForce: number;
+
+   gotGroundNormal: boolean = false;
+
+
+   constructor({ collider, collidable, groundNormalThreshold = 0.57 }: { collider: EllipsoidCollider; collidable: CollisionBoundNode; groundNormalThreshold?: number; }) {
+        super();
+        this.collidable = collidable;
+        this.collider = collider;
+        this.groundNormalThreshold = groundNormalThreshold;
+    }
+
+    clone(): SurfaceAgent {
+        return new SurfaceAgent({ collider: this.collider, collidable: this.collidable, groundNormalThreshold: this.groundNormalThreshold });
+    }
+
+    /*
+	lookAt( target: Vector3 ): SurfaceAgent {
+        let td = TARGET_DIRECTION;
+        td.x = target.x - this.position.x;
+        td.z =  target.z - this.position.y;
+        let d = 1/Math.sqrt(td.x * td.x * td.z * td.z);
+        td.x *= d;
+        td.z *= d;
+		this.rotation.lookAt( this.forward, td, this.up );
+		return this;
+    }
+    */
+
+
+    update(dt: number): SurfaceAgent {
+
+        var displacement = DISPLACEMENT;
+        var source = SOURCE;
+
+        source.x = this.position.x;
+        source.y = this.position.y + this.collider.radiusY;
+        source.z = this.position.z;
+
+        super.update(dt);
+
+        // || (this.gotGroundNormal) &&  .maximum_ground_normal.dotProduct(this.vel) > 0)
+        if ( !this.gotGroundNormal ) {
+            this.velocity.y -= this.gravForce;
+        }
+        if (this.velocity.y > 0) this.velocity.y = 0;
+
+        displacement.x = this.velocity.x * dt;
+        displacement.y = this.velocity.y * dt;
+        displacement.z = this.velocity.z * dt;
+
+        var destination = this.collider.calculateDestination(source, displacement, this.collidable);
+        var gotGroundNormal = false;
+
+       if (this.velocity.x*this.velocity.x + this.velocity.y+this.velocity.y + this.velocity.z*this.velocity.z !== 0) {
+            this.gotGroundNormal = false;
+       }
+
+        var c = this.collider.collisions;
+        this.collider.collisions = null;
+
+        var nextEvent;
+        while (c != null) {
+            nextEvent = c.next;
+            if (!gotGroundNormal && c.normal.y >= this.groundNormalThreshold ) {
+               gotGroundNormal = true;
+               // break;
+            }
+            c.next = CollisionEvent.COLLECTOR;
+            CollisionEvent.COLLECTOR = c;
+            c = nextEvent;
+        }
+        this.gotGroundNormal = gotGroundNormal;
+
+        this.position.x = destination.x;
+        this.position.y = destination.y - this.collider.radiusY;
+        this.position.z = destination.z;
+
+        return this;
+    }
+
+}
--- src/pc/yuka/yuka-entity-manager.ts
+++ src/pc/yuka/yuka-entity-manager.ts
@@ -1,87 +1,17 @@
 import { createScript, ScriptTypeBase, attrib } from "../../../lib/create-script-decorator";
 import { EntityManager } from "../../../yuka/src/core/EntityManager";
-import { MovingEntity } from "../../../yuka/src/core/MovingEntity";
-
-interface IPosNode {
-    getPosition(): pc.Vec3;
-}
-
-export type Agent = {
-    entity?: pc.Entity,
-    posNode: IPosNode
-    movingEnt: MovingEntity,
-    heightRad: number,
-
-    index: number
-}
-
-interface IYukaEntityManager {
-    entityManager: EntityManager;
-    addAgent(movingEnt:MovingEntity, posNode: IPosNode, height?:number, entity?: pc.Entity): void;
-}
-
 
 @createScript("yukaEntityManager")
-class YukaEntityManager extends ScriptTypeBase implements IYukaEntityManager {
+class YukaEntityManager extends ScriptTypeBase  {
 
     entityManager: EntityManager;
-    agents: Agent[];
 
     initialize () {
         this.entityManager = new EntityManager();
-        this.agents = [];
-    }
-
-    addAgent(movingEnt:MovingEntity, posNode: IPosNode, height:number=0, entity?: pc.Entity): void {
-        // todo
-        this.entityManager.add(movingEnt as any);
     }
 
     update(dt: number) {
-        var i, agent;
-        var agents = this.agents;
-        var pos;
-        var rot;
-        var vel;
-
-        // map agent 3D entity position back to yuka world/navmesh position (tied to foot origin) for Navmesh behaviours
-        for (i = agents.length - 1; i >= 0; i--) {
-            agent = agents[i];
-            pos = agent.posNode.getPosition();
-            agent.movingEnt.position.x = pos.x;
-            agent.movingEnt.position.y = pos.y - agent.heightRad;
-            agent.movingEnt.position.z = pos.z;
-
-            // match feedback result extVelocity for surfaceMovement back given physics (if any)
-            if (agent.entity && agent.entity.rigidbody ) { //&& !this.flowFieldBehaviour.active
-                vel = agent.entity.rigidbody.linearVelocity;
-                agent.movingEnt.velocity.x = vel.x;
-                agent.movingEnt.velocity.y = vel.y;
-                agent.movingEnt.velocity.z = vel.z;
-            }
-        }
-
         this.entityManager.update(dt);
-
-        /* // Rotation or physics cases are to be factored out to renderCallback or other systems/components with varying factors
-        for (i = agents.length - 1; i >= 0; i--) {
-            agent = agents[i];
-            let phy =  agent.entity &&  agent.entity.rigidbody;
-             vel =phy ?  agent.entity.rigidbody.linearVelocity : agent.movingEnt.velocity;
-
-            pos = agent.entity.getPosition();
-            if (vel.x*vel.x + vel.z*vel.z > 0.00000001) {
-                agent.rotationNode.lookAt(pos.x + vel.x, pos.y, pos.z + vel.z);
-           }
-            if (phy) {
-                 // yagni atm... if agent is flying, force.y should be included in
-                //force.y = agent.vehicle.velocity.y - vel.y;
-
-                //force.x *= 2500; // arbotuary value here?
-                //force.z *= 2500;
-                agent.entity.rigidbody.applyForce(agent.movingEnt.velocity.x - vel.x, 0,  agent.movingEnt.velocity.z - vel.z);
-        }
-        */
     }
 
     /*
